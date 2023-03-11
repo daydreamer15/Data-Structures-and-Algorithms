d0abf589d1cf5314dcf42c3e4f15edf7994921fd
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-03-11 20:02:31 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,65 +8,59 @@ using namespace std;
 // User function Template for C++
 class Solution {
     private:
-    void topoSort(int node, vector < pair < int, int >> adj[],
-      int vis[], stack < int > & st) {
-      //This is the function to implement Topological sort. 
-      vis[node] = 1;
-      for (auto it: adj[node]) {
-        int v = it.first;
-        if (!vis[v]) {
-          topoSort(v, adj, vis, st);
+    void toposort(int node, vector<pair<int, int>>adj[], int vis[], stack<int>&st){
+        vis[node] = 1;
+        for(auto it: adj[node]){
+            int v = it.first;
+            if(!vis[v]){
+                toposort(v, adj, vis, st);
+            }
         }
-      }
-      st.push(node);
+        st.push(node);
     }
   public:
     vector < int > shortestPath(int N, int M, vector < vector < int >> & edges) {
 
       //We create a graph first in the form of an adjacency list.
-      vector < pair < int, int >> adj[N];
-      for (int i = 0; i < M; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-        int wt = edges[i][2];
-        adj[u].push_back({v, wt}); 
-      }
-      // A visited array is created with initially 
-      // all the nodes marked as unvisited (0).
-      int vis[N] = {
-        0
-      };
-      //Now, we perform topo sort using DFS technique 
-      //and store the result in the stack st.
-      stack < int > st;
-      for (int i = 0; i < N; i++) {
-        if (!vis[i]) {
-          topoSort(i, adj, vis, st);
-        }
+      vector<pair<int, int>>adj[N];
+      for(int i = 0; i<M; i++){
+          int u = edges[i][0];
+          int v = edges[i][1];
+          int wt = edges[i][2];
+          adj[u].push_back({v, wt});
       }
       
-      vector < int > dist(N);
-      for (int i = 0; i < N; i++) {
-        dist[i] = 1e9;
+      int vis[N]={0};
+      
+      stack<int>st;
+      for(int i = 0; i<N; i++){
+         if(!vis[i])
+         toposort(i, adj, vis, st);
       }
-
+      
+      vector<int>dist(N);
+      for(int i = 0; i<N; i++){
+          dist[i] = 1e9;
+      }
+      
       dist[0] = 0;
-      while (!st.empty()) {
-        int node = st.top();
-        st.pop();
-
-        for (auto it: adj[node]) {
-          int v = it.first;
-          int wt = it.second;
-
-          if (dist[node] + wt < dist[v]) {
-            dist[v] = wt + dist[node];
+      
+      while(!st.empty()){
+          int node = st.top();
+          st.pop();
+          
+          for(auto it: adj[node]){
+              int v = it.first;
+              int wt = it.second;
+              
+              if(dist[node]+ wt < dist[v]){
+                  dist[v] = wt+dist[node];
+              }
           }
-        }
       }
-
-      for (int i = 0; i < N; i++) {
-        if (dist[i] == 1e9) dist[i] = -1;
+      
+      for(int i = 0; i<N; i++){
+          if(dist[i]==1e9)dist[i]=-1;
       }
       return dist;
     }
