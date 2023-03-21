3309d527a7924d45bc7bcb7a079e4aa7ff761b40
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-03-21 19:32:11 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -7,25 +7,26 @@ class Solution {
   public:
     int maximumPoints(vector<vector<int>>& points, int n) {
         // Code here
-        vector < vector < int > > dp(n, vector < int > (4, 0));
+      vector<int>prev(4, 0);
         
-        dp[0][0] = max(points[0][1], points[0][2]);
-        dp[0][1] = max(points[0][0], points[0][2]);
-        dp[0][2] = max(points[0][1], points[0][0]);
-        dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+        prev[0] = max(points[0][1], points[0][2]);
+        prev[1] = max(points[0][0], points[0][2]);
+        prev[2] = max(points[0][0], points[0][1]);
+        prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
         
         for(int day = 1; day<n; day++){
+            vector<int>temp(4,0);
             for(int last = 0; last<4; last++){
-                dp[day][last] = 0;
+                temp[last] = 0;
                 
                 for(int task = 0; task<=2; task++){
-                    if(task!=last) {
-                    int activity = points[day][task] + dp[day-1][task];
-                    dp[day][last] = max(dp[day][last], activity);}
+                    if(task!=last)
+                    temp[last] = max(temp[last], points[day][task]+prev[task]);
                 }
             }
+            prev = temp;
         }
-        return dp[n-1][3];
+        return prev[3];
     }
 };
 
