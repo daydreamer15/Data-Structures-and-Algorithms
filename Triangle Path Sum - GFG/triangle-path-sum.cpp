3fa841941e16811f58765375e9ee2160c22be45a
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-03-25 02:26:20 +0530

    Added solution - LeetHub

---
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+    int f( int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp){
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i == n-1) return triangle[i][j];
+        int down = triangle[i][j] + f(i+1, j, n, triangle, dp);
+        int dg = triangle[i][j] + f(i+1, j+1, n, triangle, dp);
+        
+        return dp[i][j]=min(down, dg);
+    }
+    int minimizeSum(int n, vector<vector<int>>& triangle) {
+        // Code here
+        vector<vector<int>>dp(n, vector<int>(n, -1));
+        
+        return f(0, 0, n, triangle, dp);
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < n; i++) {
+            vector<int> temp;
+            for (int j = 0; j <= i; j++) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            triangle.push_back(temp);
+        }
+        Solution obj;
+        cout << obj.minimizeSum(n, triangle) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
