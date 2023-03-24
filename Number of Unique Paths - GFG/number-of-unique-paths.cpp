79679b94c1839168f36bdb07776cb8de49a24e27
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-03-24 13:06:33 +0530

    Added solution - LeetHub

---
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template in C++
+
+class Solution
+{
+    public:
+    //MEMOIZATION WALA SOLUTION HAI!!
+    // int rec(int i, int j, vector<vector<int>>&dp){
+    //     if(i==0 && j==0) return 1;
+        
+    //     if(i<0 || j<0) return 0;
+        
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int up = rec(i-1, j, dp);
+    //     int left = rec(i, j-1, dp);
+        
+    //     return dp[i][j] = up+left;
+    // }
+    // //Function to find total number of unique paths.
+    // int NumberOfPath(int a, int b)
+    // {
+    //     //code here
+    //     vector<vector<int>>dp(a, vector<int>(b, -1));
+    //     return rec(a-1, b-1, dp);
+        
+    // }
+    
+    
+    //TABULATION WALA SOLUTION HAI!!
+    int NumberOfPath(int a, int b){
+        vector<vector<int>>dp(a, vector<int>(b, -1));
+        
+        
+        
+        for(int i = 0; i<a; i++){
+            for(int j = 0; j<b; j++){
+                if(i==0 && j==0){
+                    dp[0][0] = 1;
+                    continue;
+                }
+                
+                int up = 0, left = 0;
+                
+                if(i>0) up = dp[i-1][j];
+                
+                if(j>0) left = dp[i][j-1];
+                
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[a-1][b-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking dimensions of the matrix
+        int a,b;
+        cin>>a>>b;
+        Solution ob;
+        //calling NumberOfPath() function
+        cout << ob.NumberOfPath(a,b) << endl;
+    }
+}
+
+
+// } Driver Code Ends
