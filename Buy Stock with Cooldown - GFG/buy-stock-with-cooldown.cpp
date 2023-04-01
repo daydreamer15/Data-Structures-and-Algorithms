4b3f74d798f6aa9fd7a405b1706c318660faa512
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-04-01 18:07:43 +0530

    Added solution - LeetHub

---
+++ Buy Stock with Cooldown - GFG/buy-stock-with-cooldown.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long getAns(vector<long long>&Arr, int ind, int buy, int n, vector<vector<int>> &dp ){
+
+    if(ind>=n) return 0; //base case
+    
+    if(dp[ind][buy]!=-1)
+        return dp[ind][buy];
+        
+    int profit;
+    
+    if(buy==0){
+        profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind]+getAns(Arr,ind+1,1,n,dp));
+    }
+    
+    if(buy==1){
+        profit = max(0+getAns(Arr,ind+1,1,n,dp), Arr[ind]+getAns(Arr,ind+2,0,n,dp));
+    }
+    
+    return dp[ind][buy] = profit;
+}
+    long long maximumProfit(vector<long long>&prices, int n) {
+        // Code here
+        
+        vector<vector<int>>dp(n+1, vector<int>(2, -1));
+        
+        int ans = getAns(prices,0,0, n, dp);
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
