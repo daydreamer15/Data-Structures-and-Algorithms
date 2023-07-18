866b9da2445933a9091275efa1b0a67f549e5e0c
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-07-18 20:05:59 +0530

    Added solution - LeetHub

---
+++ Paths to reach origin - GFG/paths-to-reach-origin.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+
+const int mod = 1e9+7;
+int f(int x, int y, vector<vector<int>>&dp){
+    if(x==0 && y==0) return 1;
+    
+    else if(x<0 || y<0) return 0;
+    
+    if(dp[x][y]!=-1) return dp[x][y];
+    
+    int up = f(x-1, y, dp);
+    int left = f(x, y-1, dp);
+    
+    return dp[x][y] = (up+left)%mod;
+}
+    int ways(int x, int y)
+    {
+        //code here.
+        vector<vector<int>>dp(x+1, vector<int>(y+1,-1));
+        return f(x, y, dp)%mod;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        Solution ob;
+        cout<<ob.ways(x,y)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
