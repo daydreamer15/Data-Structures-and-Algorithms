f98eb3144a0d71c8c9596da8bb15ad5d66fba65e
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-08-04 23:36:49 +0530

    Added solution - LeetHub

--- Merge two strings - GFG/merge-two-strings.cpp
+++ Merge two strings - GFG/merge-two-strings.cpp
@@ -25,21 +25,19 @@ int main()
 string merge (string S1, string S2)
 {
     // your code here
-    int i = 0, j = 0;
-    string res = "";
+    int i = 0; 
+    int j = 0;
     
     while(i<S1.size() && j<S2.size()){
-        res+=S1[i++];
-        res+=S2[j++];
-    }
-    
-    while(i<S1.size()){
-        res+=S1[i++];
+        S1.insert(i+1, 1, S2[j]);
+        i+=2;
+        j++;
     }
     
     while(j<S2.size()){
-        res+=S2[j++];
+        S1+=S2[j];
+        j++;
     }
     
-    return res;
+    return S1;
 }
