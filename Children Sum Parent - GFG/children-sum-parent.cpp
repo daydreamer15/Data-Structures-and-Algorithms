1a3af4a6c16da737cdc7589685c08923f56d1c60
daydreamer15 <96332417+daydreamer15@users.noreply.github.com>
2023-10-17 15:38:57 +0530

    Added solution - LeetHub

--- Children Sum Parent - GFG/children-sum-parent.cpp
+++ Children Sum Parent - GFG/children-sum-parent.cpp
@@ -109,17 +109,14 @@ class Solution{
     {
      // Add your code here
      if(root==NULL) return 1;
-     
-     if(root->left==NULL && root->right==NULL) return 1;
+        
+    if(root->left==NULL && root->right==NULL) return 1;    
      
      int sum = 0;
      if(root->left) sum+=root->left->data;
      if(root->right) sum+=root->right->data;
      
-     if(root->data == sum && isSumProperty(root->left) && isSumProperty(root->right)) return 1;
-     
-     return 0;
-     
+     return (root->data==sum && isSumProperty(root->left) && isSumProperty(root->right));
     }
 };
 
